b3c125a0129f5693c0fe43cedaeffc5476313ca0
AndersonRochaJC <jmcreggae@gmail.com>
2021-09-04 16:43:29 -0300

    Soluçao exercicio Nomecompleto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tarefa_NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome;
+            Console.BackgroundColor = ConsoleColor.Red;
+             Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Digite seu nome...: ");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            nome = Console.ReadLine();
+
+            Console.BackgroundColor = ConsoleColor.Red;
+             Console.BackgroundColor = ConsoleColor.Red;
+             Console.Write("Digite seu sobrenome...: ");
+             Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+             sobrenome = Console.ReadLine();
+             
+            Console.BackgroundColor = ConsoleColor.Green;
+             Console.WriteLine($"{nome} {sobrenome}");
+            Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
+             Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+
+                     }
+    }
+}
